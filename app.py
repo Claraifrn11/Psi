7f275e2a38449a0aad835d8704da0bd0a79aecbe
Claraifrn11 <162193322+Claraifrn11@users.noreply.github.com>
2025-09-16 20:45:18 +0000

    Aula do dia 16

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask, render_template
 
 @app.route('/')
 def index():
-    return 'Olá Mundo!'
+    return 'Olá Mundo'
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/contato')
+def.contato():
+    return "<h1> alba...</h1>"
+@app.route(/exemplo)
+def exemplo ():
+    return render_template('exemplo.html')
+@app.route('exemplo2')
+def exemplo2():
+     return render_template('exemplo2.html')
